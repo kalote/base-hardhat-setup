85765bc9d469654f81d15c224f9809be25715ba8
Johann BICH <2253470+kalote@users.noreply.github.com>
2023-09-28 13:49:30 +0200

    fix hardhat gas issue

--- scripts/deployEOA.ts
+++ scripts/deployEOA.ts
@@ -1,10 +1,17 @@
-import { ethers } from "hardhat";
-import * as dotenv from "dotenv";
+import { ethers } from 'hardhat';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 async function main() {
-  const customToken = await ethers.deployContract("CustomToken");
+  // Hardhat has some issues with EIP 1559 settings, so we force it
+  // See this issue for more info: https://github.com/NomicFoundation/hardhat/issues/3418
+  const { maxFeePerGas, maxPriorityFeePerGas } = await ethers.provider.getFeeData();
+  const customToken = await ethers.deployContract('CustomToken', {
+    maxFeePerGas,
+    maxPriorityFeePerGas,
+    type: 2,
+  });
 
   await customToken.waitForDeployment();
   const CustomTokenAddress = await customToken.getAddress();
